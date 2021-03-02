905f89b44c96a85dab449f92545cd414a16a1ba7
WilliamD47 <69767121+WilliamD47@users.noreply.github.com>
2021-03-02 10:32:52 +0000

    Added Minecraft server support

--- main.py
+++ main.py
@@ -8,13 +8,14 @@ import time
 from sys import platform
 from plyer import notification
 import subprocess
-
+from mcstatus import MinecraftServer
 
 
 
 
 try:
-    channelink = input("Please enter your YouTube channel link. Do NOT input a URL like https://www.youtube.com/williamd47 Instead, input the URL you get by going to your profile picture on YouTube, then 'Your Channel'. \n")
+    channelink = input("Please enter your YouTube channel link. Do NOT input a URL like https://www.youtube.com/williamd47 Instead, input the URL you get by going to your profile picture, then 'Your Channel'. \n")
+    ip = input("Please enter a Minecraft Server IP \n")
 except Exception:
     print("Error. Please re run the program and try again.")
     exit()
@@ -28,7 +29,7 @@ except Exception:
 
 print(ytId + " is your YouTube ID. Sending a test notification.")
 def getSubs(ytId):
-    key = ""
+    key = "your api key"
     ytId = "UCgx3cwTPCFWEe7GAe_X8-1Q"
     data = urllib.request.urlopen("https://www.googleapis.com/youtube/v3/channels?part=statistics&id=" + ytId + "&fields=items/statistics/subscriberCount&key=" + key).read()
     subs = json.loads(data)["items"][0]["statistics"]["subscriberCount"]
@@ -49,12 +50,20 @@ notify("test","testnotif")
 
 
 while True:
+    oldmembers1 = MinecraftServer.lookup(ip)
+    oldmembers2 = oldmembers1.status()
+    oldmembersonline = oldmembers2.players.online
     oldsubs = getSubs(ytId)
     time.sleep(10)
     subs = getSubs(ytId)
-    print(f"old {oldsubs} new {subs}")
+    members1 = MinecraftServer.lookup(ip)
+    members2 = members1.status()
+    membersonline = members2.players.online
     try:
+        if oldmembersonline != membersonline:
+            notify(title="Member logon/logoff",message=f"You now have {membersonline} players online")
         if subs > oldsubs:
             notify(title="New Subscriber",message="User, you have a new subscriber! Your subscriber count is now on "+str(subs))
+
     except Exception as e:
         pass
